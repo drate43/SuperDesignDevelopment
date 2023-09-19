17637f91600e094b0a54b12809e9099d71054124
minha-koo <kkooming2@gmail.com>
2023-09-19 19:30:45 +0900

    :sparkles: trade 페이지 생성

---
+++ sdd/src/app/trade/page.tsx
@@ -0,0 +1,15 @@
+const TradePage = () => {
+  return (
+    <main className={"main"}>
+      <h1>드가자</h1>
+      {/* 검색 인풋 필터 */}
+      {/* 배너 영역 */}
+      {/* 1차 카테고리 필터 */}
+      {/* 2차 카테고리 필터 */}
+      {/* 필터 */}
+      {/* 상품 리스트 */}
+    </main>
+  );
+};
+
+export default TradePage;
--- sdd/src/stories/atoms/dropdown/dropdown.stories.ts
+++ sdd/src/stories/atoms/dropdown/dropdown.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { DropDownMenu } from "../../../app/components/atoms/dropdown/index";
+import { DropDownMenu } from "@/components/atoms/dropdown/index";
 
 const meta: Meta<typeof DropDownMenu> = {
   title: "Atoms/DropDown",
