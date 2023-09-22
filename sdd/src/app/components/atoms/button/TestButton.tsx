8382aabee1a8433319b64cc1d5d714351eebf1e8
Julee-Kim <jlkim0323@kakao.com>
2023-09-22 09:09:39 +0900

    button component - 작업 중

--- sdd/src/app/components/atoms/button/TestButton.tsx
+++ sdd/src/app/components/atoms/button/TestButton.tsx
@@ -1,32 +1,41 @@
 import styles from './TestButtobn.module.scss'
 
-type Ttype = 'default' | 'primary' | 'black' | 'white' | 'text' | 'link'
+type Ttype = 'text' | 'link'
+type TSize = 'mini' | 'small' | 'medium'
 
 interface IButtonProps {
   type?: Ttype
   // 버튼 내용
   label?: string;
   // 버튼 배경색
-  bgColor?: string;
+  color?: string;
+  size?: TSize
+  outline?: string
   // 클릭함수
   onClick?: () => void;
 }
 
 export const TestButton = ({
-  type = 'default',
+  type = 'text',
+  color = 'default',
+  size = 'medium',
   label,
-  bgColor,
+  outline,
+  onClick,
   ...props
 }: IButtonProps) => {
+  const btnType = type ? styles[type] : ''
+  const btnColor = color ? styles[color] : ''
+  const btnSize = size ? styles[size] : ''
+  const isOutline = outline ? styles[outline] : ''
 
-
-
-  const handleButtonClick = () => {
-    alert("click");
-  };
   return (
     <>
-      <button type="button" className={`btn${type} ${styles.btn}`} {...props} style={{backgroundColor: bgColor}}>
+      <button
+        type="button"
+        className={`${styles.btn} ${btnType} ${btnColor} ${btnSize} ${isOutline}`} {...props}
+        onClick={onClick}
+      >
         {label}
       </button>
     </>
