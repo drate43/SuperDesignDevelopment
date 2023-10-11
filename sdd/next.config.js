f20ead977894c537dbce01c7befd07a0cc0133a3
김동현 <donghyun.kim@soldout.co.kr>
2023-10-11 19:46:51 +0900

    :zap: 캐러셀 메인 페이지 적용, 캐러셀 커스텀 hook 추가

--- sdd/next.config.js
+++ sdd/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 이미지 호스트 이름 임시로 추가 - carousel 컴포넌트
+  images: {
+    domains: ["image.brandi.me"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- sdd/src/app/trade/page.tsx
+++ sdd/src/app/trade/page.tsx
@@ -1,12 +1,39 @@
+"use client";
+
+import React from "react";
 import styles from "./trade.module.scss";
+import CarouselBox from "@/components/atoms/carousel";
+import useCarousel from "@/hooks/carousel/useCarousel";
+
 const TradePage = () => {
+  const [carouselList] = useCarousel();
+
+  // 캐러셀 세부 컴포넌트
+  const Slide = carouselList.map((data, index) => {
+    const { image_path, alt } = data;
+
+    return (
+      <CarouselBox.Slide key={index}>
+        <CarouselBox.Image src={image_path} alt={alt} fill />
+      </CarouselBox.Slide>
+    );
+  });
+
   return (
     <main className={styles.tradeMain}>
       {/* 검색 인풋 필터 */}
       <div>검색 인풋 필터</div>
-      <br />
-      {/* 배너 영역 */}
-      <div>배너 영역</div>
+      {/*배너 영역*/}
+      <CarouselBox height={200} itemList={carouselList}>
+        <CarouselBox.SlideBox
+          spaceBetween={50}
+          slidesPerView={"auto"}
+          onSlideChange={(data) => console.log("캐러샐 슬라이드", data)}
+          onSwiper={(swiper) => console.log("캐러셀 생성 :::", swiper)}
+        >
+          {Slide}
+        </CarouselBox.SlideBox>
+      </CarouselBox>
       <br />
       {/* 1차 카테고리 필터 */}
       <div>1차 카테고리 필터</div>
